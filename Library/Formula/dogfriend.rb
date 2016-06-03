b24fe332a349e0c427d8d83c69eb4fd16374eacb
Gary Hetzel <garyhetzel@gmail.com>
2016-06-03 10:59:23 -0400

    Publishing dogfriend 0.1.4

--- Library/Formula/dogfriend.rb
+++ Library/Formula/dogfriend.rb
@@ -1,13 +1,13 @@
 class Dogfriend < Formula
   desc "A command line utility for testing Chef changes"
   homepage "https://github.com/DataDog/dogfriend"
-  url "https://github.com/DataDog/dogfriend/releases/download/v0.1.4/dogfriend-0.1.4-darwin-x86_64.zip"
+  version '0.1.4'
+  url "http://devops-public.s3.amazonaws.com/dogfriend-#{ version }-darwin-x86_64.zip"
   sha256 "c4fd3ce0f296d94f811cf8b3388a4f2d126a4466a10ecc85ed22ef7858ce28ea"
 
   def install
-    unzip_dir = "dogfriend-0.1.4-darwin-x86_64"
-    prefix.install Dir["#{unzip_dir}/*"]
-    bin.install_symlink prefix+"dogfriend-0.1.4-darwin-x86_64" => "dogfriend"
+    prefix.install Dir["*"]
+    bin.install_symlink prefix+"dogfriend-#{ version }-darwin-x86_64" => "dogfriend"
   end
 
   test do
